259633b8833ec69bfa8dab4db51c5267d5105330
unknown <m.riyyan.firdaus@gmail.com>
2022-08-13 18:27:43 +0700

    perbaikan minor (me)

--- pages/activities/index.js
+++ pages/activities/index.js
@@ -8,12 +8,6 @@ const Activities = ({dataActivities}) => {
    const router = useRouter();
    const slug = str => str.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
 
-   useEffect(() => {
-      dataActivities.map(data => {
-         console.log(data.properties)
-      })
-   },[])
-
    return (
       <Layout pageTitle="Kegiatan">
          <div className="mt-16 min-h-screen">
@@ -21,7 +15,6 @@ const Activities = ({dataActivities}) => {
                <h2 className="mb-12 font-extrabold text-center text-4xl text-slate-700">ELDEEM ACTIVITIES</h2>
                <div className="grid md:grid-cols-3 grid-cols-1 gap-y-16 justify-items-center">
                   {
-<<<<<<< HEAD
                      dataActivities.map(activity => (
                         <div className="md:w-[86] w-80 cursor-pointer bg-white border-2 border-slate-500 rounded-lg hover:shadow-lg p-5" onClick={() => router.push(`/activities/${slug(activity.properties.Title.title[0].plain_text)}`)} key={activity.id}>
                            <figure>
@@ -30,11 +23,6 @@ const Activities = ({dataActivities}) => {
                                  <Image src={activity.properties.Image.files[0]?.file.url} className="max-w-4xl rounded-lg object-cover object-center" alt={activity.properties.Title.title[0]?.plain_text} width={600} height={435} /> : activity.properties.Image.files[0]?.hasOwnProperty('external') ? <Image src={activity.properties.Image.files[0]?.external.url} className="max-w-4xl rounded-lg object-cover object-center" alt={activity.properties.Title.title[0]?.plain_text} width={600} height={435} /> : <Image src="https://source.unsplash.com/600x435?islamic" className="max-w-4xl rounded-lg object-cover object-center" alt={activity.properties.Title.title[0]?.plain_text} width={600} height={435} />
                               }
                            </figure>
-=======
-                     dataActivities && dataActivities.map(activity => (
-                        <div className="md:w-[86] w-80 cursor-pointer bg-white border-2 border-slate-500 rounded-lg hover:shadow-lg p-5" onClick={() => router.push(`/activities/${slug(activity.properties.Title.title[0].plain_text)}`)} key={activity.id}>
-                           <Image src="https://source.unsplash.com/1080x1350?building" className="max-w-4xl rounded-lg object-cover object-center" width={600} height={435} />
->>>>>>> b0ad968ac5056ee8e388e8c49251aa1f88615f1f
                            <div className="mt-4">
                               <h2 className="text-xl text-slate-700 font-bold">{activity.properties.Title.title[0].plain_text}</h2>
                               <p className="badge badge-accent text-secondary mt-1">{activity.properties.Category.select.name}</p>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,10 +8,6 @@ const Blog = ({dataBlog}) => {
    const router = useRouter();
    const slug = str => str.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
 
-   useEffect(() => {
-      console.log(dataBlog)
-   },[])
-
    return (
       <Layout pageTitle="Blog">
          <div className="mt-16 min-h-screen">
@@ -21,16 +17,12 @@ const Blog = ({dataBlog}) => {
                   {
                      dataBlog && dataBlog.map(blog => (
                         <div className="md:w-96 w-80 cursor-pointer rounded-lg hover:shadow-lg p-6" onClick={() => router.push(`/blog/${slug(blog.properties.Title.title[0].plain_text)}`)} key={blog.id}>
-<<<<<<< HEAD
                            <figure>
                               {
                                  blog.properties.Image.files[0]?.hasOwnProperty('file') ?
                                  <Image src={blog.properties.Image.files[0]?.file.url} className="max-w-4xl rounded-lg" alt={blog.properties.Title.title[0]?.plain_text} width={600} height={325} /> : blog.properties.Image.files[0]?.hasOwnProperty('external') ? <Image src={blog.properties.Image.files[0]?.external.url} className="max-w-4xl rounded-lg" alt={blog.properties.Title.title[0]?.plain_text} width={600} height={325} /> : <Image src="https://source.unsplash.com/600x325?islamic" className="max-w-4xl rounded-lg" alt={blog.properties.Title.title[0]?.plain_text} width={600} height={325} />
                               }
                            </figure>
-=======
-                           <Image src="https://source.unsplash.com/600x325?building" className="max-w-4xl rounded-lg" width={600} height={325} />
->>>>>>> b0ad968ac5056ee8e388e8c49251aa1f88615f1f
                            <p className="text-slate-500">{dateFormat(blog.properties.Published_Date.date.start)}</p>
                            <h2 className="text-xl text-slate-700 font-bold">{blog.properties.Title.title[0].plain_text}</h2>
                            <p className="text-slate-300 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
