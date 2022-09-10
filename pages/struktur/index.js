eb389789ffe81dd3744a1e20e1bc0a7b878edf25
riyyanfir <60066061+riyyanfir@users.noreply.github.com>
2022-09-10 07:12:57 +0700

    Update index.js
    Penambahan struktur organisasi LDM

--- pages/struktur/index.js
+++ pages/struktur/index.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Layout from "../../components/Layout";
+import chartLDM from '../../public/chartLDM.jpg';
 
 const Struktur = () => {
    return (
@@ -6,6 +8,7 @@ const Struktur = () => {
          <div className="mt-16 min-h-screen">
             <div className="container mx-auto py-16 md:px-12">
                <h2 className="mb-12 font-extrabold text-center text-4xl text-slate-700">Struktur Organisasi</h2>
+                  <Image src={chartLDM} width={1788} height={601} />
             </div>
          </div>
       </Layout>
