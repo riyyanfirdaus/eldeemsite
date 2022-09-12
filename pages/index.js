f5a4c35771cc9fd3e7766d5cb4ef03da2d2a5f06
unknown <m.riyyan.firdaus@gmail.com>
2022-09-12 15:52:22 +0700

    Penambahan dan perubahan section program unggulan

--- pages/index.js
+++ pages/index.js
@@ -29,20 +29,27 @@ export default function Home({dataAct, dataBlog}) {
             <Image src="https://res.cloudinary.com/devrbr7ot/image/upload/v1662140087/Eldeem/char_2_v6o0rk.png" className="max-w-sm rounded-lg" width={460} height={600} />
           </div>
           <div className="sm:ml-20 sm:px-0 px-6 sm:text-left text-center">
-            <h2 className="sm:text-4xl text-3xl text-secondary sm:mb-10 mb-12 font-extrabold">2 Program Unggulan</h2>
-            <div className="mb-12 sm:mb-2 mt-6 sm:mt-2">
+            <h2 className="sm:text-4xl text-3xl text-secondary sm:mb-10 mb-12 font-extrabold">3 Program Unggulan</h2>
+            <div className="mb-12 sm:mb-6 mt-6 sm:mt-2">
               <div className="flex items-center mb-4 sm:justify-start justify-center">
                 <div className="sm:w-12 sm:h-12 w-10 h-10 bg-secondary rounded-full flex mr-4"><span className="sm:text-3xl text-2xl text-primary m-auto font-semibold">1</span></div>
                 <h3 className="sm:text-3xl text-2xl font-semibold text-secondary">Pembinaan</h3>
               </div>
               <p className="text-slate-600 sm:text-lg text-md">Terbinanya anggota LDM dengan memiliki wawasan keislaman yang luas, berakhlakul karimah, dan memiliki pengetahuan dalam berdakwah.</p>
             </div>
-            <div className="mb-12 sm:mb-2">
+            <div className="mb-12 sm:mb-6">
               <div className="flex items-center mb-4 sm:justify-start justify-center">
                 <div className="sm:w-12 sm:h-12 w-10 h-10 bg-secondary rounded-full flex mr-4"><span className="sm:text-3xl text-2xl text-primary m-auto font-semibold">2</span></div>
                 <h3 className="sm:text-3xl text-2xl font-semibold text-secondary">Pengabdian</h3>
               </div>
               <p className="text-slate-600 sm:text-lg text-md">Adanya program pengabdian terhadap masjid, masyarakat kampus, dan masyarakat luas, sehingga dapat memberikan dampak kebaikan disegala lini.</p>
+            </div>
+            <div className="mb-12 sm:mb-2">
+              <div className="flex items-center mb-4 sm:justify-start justify-center">
+                <div className="sm:w-12 sm:h-12 w-10 h-10 bg-secondary rounded-full flex mr-4"><span className="sm:text-3xl text-2xl text-primary m-auto font-semibold">3</span></div>
+                <h3 className="sm:text-3xl text-2xl font-semibold text-secondary">Pemberdayaan</h3>
+              </div>
+              <p className="text-slate-600 sm:text-lg text-md">Memberikan ruang bagi anggota untuk mengeksplor wawasan, kemampuan, dan potensi yang dimiliki.</p>
             </div>
           </div>
         </div>
